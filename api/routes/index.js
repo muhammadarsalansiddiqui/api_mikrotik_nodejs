2024ba781a54077241dbe0e04d762b8972739aae
nemerfrozen <nemerfrozen@gmail.com>
2016-03-05 18:51:47 -0500

    add express api folder

---
+++ api/routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+var MikroNode = require('mikronode');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+
+var connection = new MikroNode('192.168.10.1','admin','1130668439/*/*');
+connection.connect(function(conn) {
+
+   var chan=conn.openChannel();
+   conn.closeOnDone(true);
+   chan.write('/queue/simple/print',function() {
+      chan.closeOnDone(true);
+      chan.on('done',function(data) {
+
+var listusers=[];
+
+         var parsed = MikroNode.parseItems(data);
+
+        // parsed.forEach(function(item) {
+
+            var name=(JSON.stringify(parsed));
+            //var name=(JSON.stringify(item.name));
+           res.write(name);
+          // });
+
+        // res.write(listusers);
+         res.end();
+ 
+      }); 
+   }); 
+}); 
+
+
+
+  
+});
+
+
+
+
+module.exports = router;
